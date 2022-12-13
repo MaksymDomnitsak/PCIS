e3be22ee2f48544657b5aba84fcb6ad657e3e52c
maximuse.domn <user.maximuse@gmail.com>
2022-12-13 08:52:43 +0200

    Add files via upload

---
+++ Lab7/Shared/Position.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab7
+{
+    public partial class Position
+    {
+        public Position()
+        {
+            UniversityTeachers = new HashSet<UniversityTeacher>();
+        }
+
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [StringLength(64)]
+        [Unicode(false)]
+        public string Name { get; set; } = string.Empty;
+
+        [InverseProperty("PositionNameNavigation")]
+        public virtual ICollection<UniversityTeacher> UniversityTeachers { get; set; }
+    }
+}
---
+++ Lab7/Shared/UniversityTeacher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab7
+{
+    public partial class UniversityTeacher
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [StringLength(32)]
+        [Unicode(false)]
+        public string FirstName { get; set; } = null!;
+        [StringLength(32)]
+        [Unicode(false)]
+        public string LastName { get; set; } = null!;
+        [StringLength(32)]
+        [Unicode(false)]
+        public string PatronymicName { get; set; } = null!;
+        public int WorkPlace { get; set; }
+        public int PositionName { get; set; }
+        public int FirstSubjectName { get; set; }
+        public int? SecondSubjectName { get; set; }
+        public int PositionHourlyRate { get; set; }
+        public int CountReadHours { get; set; }
+        [StringLength(128)]
+        [Unicode(false)]
+        public string HomeAddress { get; set; } = null!;
+        [Column(TypeName = "text")]
+        public string Characteristics { get; set; } = null!;
+
+        [ForeignKey("FirstSubjectName")]
+        [InverseProperty("UniversityTeacherFirstSubjectNameNavigations")]
+        public virtual Subject? FirstSubjectNameNavigation { get; set; }
+        [ForeignKey("PositionName")]
+        [InverseProperty("UniversityTeachers")]
+        public virtual Position? PositionNameNavigation { get; set; }
+        [ForeignKey("SecondSubjectName")]
+        [InverseProperty("UniversityTeacherSecondSubjectNameNavigations")]
+        public virtual Subject? SecondSubjectNameNavigation { get; set; }
+        [ForeignKey("WorkPlace")]
+        [InverseProperty("UniversityTeachers")]
+        public virtual Workplace? WorkPlaceNavigation { get; set; }
+    }
+}
